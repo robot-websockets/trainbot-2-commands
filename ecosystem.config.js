edbf81d1b7c47740e064652aa33215b2ecaa9ec1
philstenning <philstenning@hotmail.com>
2019-02-01 12:42:25 +0000

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,14 +9,14 @@ module.exports = {
         {
             name: 'barcode',
             script: 'C:/Dev/2019/hackhorsham/_pi_3/barcode-server/server.py',
-            args: ['-W', '192.168.55.1:5001', '-V', '192.168.55.12:8081'],
+            args: ['-W', '192.168.55.11:5001', '-V', '192.168.55.12:8081'],
             autorestart: true,
             watch: false
         },
         {
             name: 'opencv-server',
             script: 'C:/Dev/2019/hackhorsham/_pi_3/openCV-server/server.py',
-            args: ['-W', '192.168.55.1:5001', '-V', '192.168.55.12:8081'],
+            args: ['-W', '192.168.55.11:5001', '-V', '192.168.55.12:8081'],
             autorestart: true,
             watch: false
         }
