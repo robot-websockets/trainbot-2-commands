9b6ee6d786de3da773392b5cbc0fbc5d58f9a79f
philstenning <philstenning@hotmail.com>
2019-02-01 12:39:21 +0000

    change for user c

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,22 @@
 module.exports = {
     apps: [
         {
-            name: 'brain',
-            script: '/home/pi/trainbot-2/main-server/app.js',
+            name: 'main_server',
+            script: 'C:/Dev/2019/hackhorsham/_pi_3/main-server/app.js',
             autorestart: true,
             watch: false
         },
         {
             name: 'barcode',
-            script: '/home/pi/trainbot-2/barcode-server/server.py',
-            interpreter: '/usr/bin/python3.5',
+            script: 'C:/Dev/2019/hackhorsham/_pi_3/barcode-server/server.py',
+            args: ['-W', '192.168.55.1:5001', '-V', '192.168.55.12:8081'],
             autorestart: true,
             watch: false
         },
         {
             name: 'opencv-server',
-            script: '/home/pi/trainbot-2/openCV-server/server.py',
-            interpreter: '/usr/bin/python3.5',
+            script: 'C:/Dev/2019/hackhorsham/_pi_3/openCV-server/server.py',
+            args: ['-W', '192.168.55.1:5001', '-V', '192.168.55.12:8081'],
             autorestart: true,
             watch: false
         }
